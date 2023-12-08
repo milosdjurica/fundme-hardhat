18a6b2073ec99bed8e837c5a4baac55d4c0294d3
milosdjurica <milosdjurica13@gmail.com>
2023-12-09 00:05:29 +0100

    added namedAccounts

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,5 +1,6 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
+import { network } from "hardhat";
 
 const deployFunc: DeployFunction = async function (
 	hre: HardhatRuntimeEnvironment,
@@ -7,5 +8,12 @@ const deployFunc: DeployFunction = async function (
 	const { deployments, getNamedAccounts } = hre;
 	const { deploy, log } = deployments;
 	const { deployer } = await getNamedAccounts();
+	const chainId = network.config.chainId;
+
+	const fundMe = await deploy("FundMe", {
+		from: deployer,
+		args: [], // put price feed address
+		log: true,
+	});
 };
 export default deployFunc;
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,7 +5,17 @@ import "hardhat-deploy";
 
 const config: HardhatUserConfig = {
 	solidity: "0.8.20",
-	namedAccounts: {},
+	namedAccounts: {
+		deployer: {
+			default: 0,
+			// chainId : accountNumber
+			// 1: 0,
+			// 31337: 3,
+		},
+		user: {
+			default: 1,
+		},
+	},
 };
 
 export default config;
