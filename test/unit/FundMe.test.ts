89fab693a8f1c63c63ae4e6c08f78717785cc6e4
milosdjurica <milosdjurica13@gmail.com>
2023-12-09 13:43:44 +0100

    test withdraw with multiple funders

--- test/unit/FundMe.test.ts
+++ test/unit/FundMe.test.ts
@@ -76,5 +76,48 @@ describe("FundMe", () => {
 				endingDeployerBalance + totalGasCost,
 			);
 		});
+
+		it("allows withdraw with multiple funders", async () => {
+			// ARRANGE
+			const accounts = await ethers.getSigners();
+			// ! start from 1 to avoid taking deployer
+			for (let i = 1; i < accounts.length; i++) {
+				const fundMeConnectedContract = fundMe.connect(accounts[i]);
+				await fundMeConnectedContract.fund({ value: sendValue });
+			}
+			const startingFundMeBalance = await ethers.provider.getBalance(
+				fundMe.getAddress(),
+			);
+			const startingDeployerBalance =
+				await ethers.provider.getBalance(deployer);
+
+			// ACT
+			const transactionResponse = await fundMe.withdraw();
+			const transactionReceipt = await transactionResponse.wait(1);
+
+			const { gasUsed, gasPrice } = transactionReceipt!;
+			const totalGasCost = gasUsed * gasPrice;
+
+			// ASSERT
+			const endingFundMeBalance = await ethers.provider.getBalance(
+				fundMe.getAddress(),
+			);
+			const endingDeployerBalance = await ethers.provider.getBalance(deployer);
+			assert.equal(
+				startingDeployerBalance + startingFundMeBalance,
+				endingDeployerBalance + totalGasCost,
+			);
+
+			await expect(fundMe.getFunder(0)).to.be.reverted;
+			// await expect(fundMe.fund()).to.be.revertedWith(
+			// 	"You need to spend more ETH!",
+			// );
+			for (let i = 0; i < accounts.length - 1; i++) {
+				assert.equal(
+					Number(await fundMe.getAddressToAmountFunded(accounts[i].address)),
+					0,
+				);
+			}
+		});
 	});
 });
