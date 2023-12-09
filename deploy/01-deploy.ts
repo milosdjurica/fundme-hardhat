ad4b7c5a92e31a8e718ae9dd3e1a95ccee5a6425
milosdjurica <milosdjurica13@gmail.com>
2023-12-09 01:45:03 +0100

    finished deploy script

--- deploy/01-deploy.ts
+++ deploy/01-deploy.ts
@@ -1,21 +1,31 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
-import { networkConfig } from "../helper-hardhat-config";
+import { developmentChains, networkConfig } from "../helper-hardhat-config";
 
 const deployFunc: DeployFunction = async function (
 	hre: HardhatRuntimeEnvironment,
 ) {
 	const { deployments, getNamedAccounts, network } = hre;
-	const { deploy, log } = deployments;
+	const { deploy, log, get } = deployments;
 	const { deployer } = await getNamedAccounts();
 	const chainId = network.config.chainId || 11155111;
 
-	const ethUsdPriceFeedAddress = networkConfig[chainId].ethUsdPriceFeed;
+	let ethUsdPriceFeedAddress;
+	// ! if on dev chain -> get mock address, ELSE get from network
+	if (developmentChains.includes(network.name)) {
+		const ethUsdAggregator = await get("MockV3Aggregator");
+		ethUsdPriceFeedAddress = ethUsdAggregator.address;
+	} else {
+		ethUsdPriceFeedAddress = networkConfig[chainId].ethUsdPriceFeed;
+	}
 
 	const fundMe = await deploy("FundMe", {
 		from: deployer,
-		args: [], // put price feed address
+		args: [ethUsdPriceFeedAddress], // put price feed address
 		log: true,
 	});
+
+	log("===================================================================");
 };
 export default deployFunc;
+deployFunc.tags = ["all", "fundMe"];
